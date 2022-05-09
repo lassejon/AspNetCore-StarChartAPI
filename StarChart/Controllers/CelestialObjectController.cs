31c95cdb2c6a4d6964212e3fd3318d4843704051
AzureAD\LasseJonMarquardtFre <lasse_frederiksen@danfoss.com>
2022-05-09 14:29:36 +0200

    Added GET methods to retrieve a CelestialObject with id, name and all of them

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -13,5 +13,31 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+            if (celestialObject == null) return NotFound();
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObject = _context.CelestialObjects.Find(name);
+            if (celestialObject == null) return NotFound();
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects;
+
+            return Ok(celestialObjects);
+        }
     }
 }
